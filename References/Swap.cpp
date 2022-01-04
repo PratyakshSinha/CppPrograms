14313dfc23671c867167be80bdfd30aa55422016
Pratyaksh Sinha <pratyakshsinha1234@gmail.com>
2022-01-04 23:00:58 +0530

    swap2

--- References/Swap.cpp
+++ References/Swap.cpp
@@ -1,19 +1,21 @@
 #include<iostream>
 using namespace std;
 
-void swapByReference(int *x, int *y)
+void swapByReference(int *a, int *b)
 {
     int temp;
-    temp = *x;
-    *x = *y;
-    *y = temp;
+    cout<<a<<endl<<b<<endl<<&temp<<endl;
+    temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 void swapByValue(int , int);
 
 int main()
 {
-    int x=5, y=10;
+    int x, y;
+    cout<<&x<<endl<<&y<<endl;
     cout<<"Current Value of x = "<<x<<endl<<"Current Value of y = "<<y<<endl;
     swapByReference(&x, &y);
     cout<<"Current Value of x = "<<x<<endl<<"Current Value of y = "<<y<<endl;
