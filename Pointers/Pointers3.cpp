4c2d059ef124d2ce16f6cae40b5854f24b8d45c9
Pratyaksh Sinha <pratyakshsinha1234@gmail.com>
2022-01-29 21:23:52 +0530

    commit

--- Pointers/Pointers3.cpp
+++ Pointers/Pointers3.cpp
@@ -3,24 +3,22 @@ using namespace std;
 
 int main()
 {
-    int luckyNumbers[5];
+    int luckyNumbers[5] = {1,3,5,7,9};
 
-/*     cout<<luckyNumbers<<endl;
+    cout<<luckyNumbers<<endl;
     cout<<&luckyNumbers[0]<<endl;
     cout<<luckyNumbers[2]<<endl;
-    cout<<*(luckyNumbers + 2)<<endl; */
-
+    cout<<*(luckyNumbers + 2)<<endl; 
+/* 
     for(int i =0; i<5; i++){
         cout<<"Number: ";
         cin>> luckyNumbers[i];
     }
-
+ */
     for(int i =0; i<5; i++){
         cout << *(luckyNumbers+i) << " ";
     }
 
-
-    
     system("pause>0");
     return 0;
 }
