5a52e70be9e45a0bd1aa7978d3448304f1d3b61f
Pratyaksh Sinha <pratyakshsinha1234@gmail.com>
2022-02-19 01:22:04 +0530

    HackerRank

---
+++ HackerRank/VirtualFunctions.cpp
@@ -0,0 +1,44 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+class Person{
+    protected:
+        string name;
+        int age;
+};
+
+class Professor:public Person{
+
+};
+
+int main(){
+
+    int n, val;
+    cin>>n; //The number of objects that is going to be created.
+    Person *per[n];
+
+    for(int i = 0;i < n;i++){
+
+        cin>>val;
+        if(val == 1){
+            // If val is 1 current object is of type Professor
+            per[i] = new Professor;
+
+        }
+        else per[i] = new Student; // Else the current object is of type Student
+
+        per[i]->getdata(); // Get the data from the user.
+
+    }
+
+    for(int i=0;i<n;i++)
+        per[i]->putdata(); // Print the required output for each object.
+
+    return 0;
+
+}
