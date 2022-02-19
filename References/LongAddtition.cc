fe1726fe571765a94b9fd72db2183e597da4b897
Pratyaksh Sinha <pratyakshsinha1234@gmail.com>
2022-02-20 00:22:31 +0530

    AddRef

---
+++ References/LongAddtition.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+    int x, y, z;
+    ll res;
+
+    cin>>x>>y>>z;
+
+    res = x*y*z;
+    cout<<res;
+
+    return 0;
+}
