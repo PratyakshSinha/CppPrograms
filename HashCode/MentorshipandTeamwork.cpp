358518585188983c62554e3f5171edfb03e65963
Pratyaksh Sinha <pratyakshsinha1234@gmail.com>
2022-02-25 23:41:42 +0530

    HashCode

---
+++ HashCode/MentorshipandTeamwork.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class projects{
+
+    string name;
+    int days, score, best_before, roles;
+    map<string, int> skills_required;
+    map<string, vector<pair<string, int>>> skills;
+
+};
+
+int main()
+{
+
+    map<string, vector<pair<string, int>>> contributor;
+    
+
+}
