03ab941fe4b3ac0fd8d47ae9268f8473d41ccbcc
Pratyaksh Sinha <pratyakshsinha1234@gmail.com>
2022-04-24 00:08:56 +0530

    V

--- GeeksForGeeks/String/RecursivelyRemoveAllAdjacentDuplicates.cc
+++ GeeksForGeeks/String/RecursivelyRemoveAllAdjacentDuplicates.cc
@@ -4,42 +4,67 @@
 #include <bits/stdc++.h>
 using namespace std;
 
- // } Driver Code Ends
-//User function template for C++
+// } Driver Code Ends
+// User function template for C++
 
-class Solution{
+class Solution
+{
 public:
+
+    // Provided by Vidhi Gambhir the Great
     string rremove(string s)
     {
-         int i=0,n=s.size();
-     string temp;
-     while(i<n)
-     {
-         int j=i+1;
-         while(s[i]==s[j]&&j<n)
-         j++;
-         if(j==i+1)temp+=s[i];
-         i=j;
-     }
-     if(s.size()==temp.size())
-     return temp;
-     else
-     return rremove(temp);
+        int n = s.length();
+
+        if (s.length() <= 1)
+        {
+            return s;
+        }
+        int flag = 0;
+        int i = 0;
+        while (i < s.length())
+        {
+            int j = i + 1;
+            if (s[i] == s[i + 1] && i + 1 < s.length())
+            {
+                while (j + 1 < s.length() && s[j] == s[j + 1])
+                {
+                    j++;
+                }
+                s.erase(i, j - i + 1);
+            }
+            else
+            {
+                i++;
+            }
+        }
+        for (int i = 0; i <= s.size() - 1;)
+        {
+            if (s[i] == s[i + 1])
+            {
+                return rremove(s);
+            }
+            else
+                i++;
+        }
+        return s;
     }
 };
 
 // { Driver Code Starts.
 
-int main() {
+int main()
+{
     int t;
     string tc;
     getline(cin, tc);
     t = stoi(tc);
-    while (t--) {
+    while (t--)
+    {
         string s;
         getline(cin, s);
         Solution ob;
         cout << ob.rremove(s) << "\n";
     }
     return 0;
-}  // } Driver Code Ends
+} // } Driver Code Ends
