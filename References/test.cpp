9215c52b3ad9c51849802033fab8f3492399e953
Pratyaksh Sinha <pratyakshsinha1234@gmail.com>
2022-09-05 03:30:39 +0530

    commit

--- References/test.cpp
+++ References/test.cpp
@@ -3,9 +3,16 @@ using namespace std;
 
 int main()
 {
-    for(int i = 0;i++<5;)
-    {
-        cout<<"HELLO"<<endl;
-    }
+    int n;
+    cin>>n;
+    int temp = n/3;
+    temp = temp*35;
+    n = n%3;
+    if(n == 1)
+        temp+=20;
+    else if(n == 2)
+        temp += 30;
+    cout<<temp;
+
     return 0;
 }
