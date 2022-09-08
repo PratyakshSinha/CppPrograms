225b3f7098634b4fa654dfebbbad381eab5b0689
Pratyaksh Sinha <pratyakshsinha1234@gmail.com>
2022-09-09 05:18:21 +0530

    Formatting

--- LeetCode/MergeTwoSortedList.cc
+++ LeetCode/MergeTwoSortedList.cc
@@ -18,7 +18,6 @@ public:
             ListNode *returnList = (list1->val<list2->val) ? list1 : list2;
             (list1->val<list2->val) ? list1 = list1->next : list2 = list2->next;
             ListNode *tail = returnList;
-
             while(list1 and list2)
             {
                 if(list1 and (list1->val < list2->val))
@@ -34,16 +33,10 @@ public:
                     list2 = list2->next;
                 }
             }
-
             if(list1)
-            {
                 tail->next = list1;
-            }
             if(list2)
-            {
                 tail->next = list2;
-            }
-
             return returnList;
         }
         else
