d23104546114d2bbf10a7768124cf003f78b176c
Pratyaksh Sinha <pratyakshsinha1234@gmail.com>
2022-10-26 03:54:37 +0530

    DSA

--- LeetCode/Permutations.cc
+++ LeetCode/Permutations.cc
@@ -33,4 +33,27 @@ public:
         helper(nums, res, ds, freq);
         return res;
     }
+
+    void helper1(int ind, vector<int>& nums, vector<vector<int>>& ans)
+    {
+        if(ind == nums.size())
+        {
+            ans.push_back(nums);
+            return;
+        }
+
+        for(int i = ind;i<nums.size();i++)
+        {
+            swap(nums[ind], nums[i]);
+            helper1(ind+1, nums, ans);
+            swap(nums[ind], nums[i]);
+        }
+    }
+
+    vector<vector<int>> permute1(vector<int>& nums)
+    {
+        vector<vector<int>> ans;
+        helper1(0, nums, ans);
+        return ans;
+    }
 };
